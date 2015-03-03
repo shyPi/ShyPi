a85c6a665f932c5f552462a1480cdd1bd5f43ef6
wxu <winnielandau@gmail.com>
2015-03-02 20:54:24 -0800

    add Slider with number indicator, default to 80 to form

--- app/assets/javascripts/shushers.js
+++ app/assets/javascripts/shushers.js
@@ -1,2 +1,22 @@
 console.log("HELLO KITTY");
 
+$(function() {
+  var ageInput = $("#ageInput");
+  var ageDisplay = $("#ageInput > div");
+  var initialValue = 80;
+  var handle = null;
+
+  var updateSliderValue = function (event, ui) {
+      handle = handle || $(".ui-slider-handle", ageInput);
+      ageDisplay.text(ui.value || initialValue)
+                .css(handle.position());
+  };
+
+  ageInput.slider({
+      min: 1, max: 100,
+      slide: updateSliderValue,
+      create: updateSliderValue,
+      value: initialValue
+  });
+
+});
